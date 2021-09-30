5f2caefe4b223b308ecf473958ad5272701012bf
Amir Hossein Maher <amiria703@gmail.com>
2021-09-30 22:23:44 +0330

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -14,6 +13,4 @@ export default {
     HelloWorld
   }
 }
-
-
 </script>
