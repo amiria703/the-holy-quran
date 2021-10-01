0659b959849beb680da2ed926bd4279aca867acb
Amir Hossein Maher <amiria703@gmail.com>
2021-10-01 14:52:52 +0330

    initial

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/1'
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/:page',
+    name: 'page',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/Page.vue')
   }
 ]
 
---
+++ src/views/Page.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page">
+    <h1>{{$route.params.page}}</h1>
+  </div>
+</template>
+
+<script>
+  const axios = require('axios');
+  axios.get('http://api.alquran.cloud/v1/page/'+this.$route.params.page+'/quran-unicode')
+          .then(function (response) {
+            // handle success
+            console.log(response);
+          })
+          .catch(function (error) {
+            // handle error
+            console.log(error);
+          })
+</script>
