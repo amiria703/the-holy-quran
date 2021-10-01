0421e8e70ef2a2c38f7691b2f56e746fa20b7532
Amir Hossein Maher <amiria703@gmail.com>
2021-10-01 15:46:24 +0330

    initial

--- src/views/Page.vue
+++ src/views/Page.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="page">
     <h1>{{$route.params.page}}</h1>
+    <p>{{this.text()}}</p>
   </div>
 </template>
 
 <script>
-  const axios = require('axios');
-  axios.get('http://api.alquran.cloud/v1/page/'+this.$route.params.page+'/quran-unicode')
-          .then(function (response) {
-            // handle success
-            console.log(response);
-          })
-          .catch(function (error) {
-            // handle error
-            console.log(error);
-          })
+  export default {
+    data: function() {
+      return {
+        page: this.$route.params.page
+
+      };
+    },
+    methods: {
+      text: function(){
+        const axios = require('axios');
+        axios.get('http://api.alquran.cloud/v1/page/'+this.page+'/quran-unicode')
+                .then(function (response) {
+                  // handle success
+                  console.log(response.data.data.ayahs[0].text);
+                  return response.data.data.ayahs[0].text
+                })
+                .catch(function (error) {
+                  // handle error
+                  console.log(error);
+                })
+      }
+    }
+  }
 </script>
