e2023a706371f1027571661bb17d8e626c425a01
Amir Hossein Maher <amiria703@gmail.com>
2021-10-08 15:49:37 +0330

    initial

--- src/views/Page.vue
+++ src/views/Page.vue
@@ -1,33 +1,26 @@
 <template>
-  <div class="page">
-    <h1>{{$route.params.page}}</h1>
-    <p>{{this.text()}}</p>
-<!--    TODO: The above code doesn't work-->
-  </div>
+    <div class="page">
+        <h1>{{$route.params.page}}</h1>
+        <div v-for="text in texts" :key="text.id">
+            <p>{{text.text}}</p>
+        </div>
+    </div>
 </template>
 
 <script>
-  export default {
-    data: function() {
-      return {
-        page: this.$route.params.page
+    const axios = require('axios');
+    export default {
+        data () {
+            return {
+                page: this.$route.params.page,
+                texts: null
+            }
+        },
+        mounted () {
+            axios
+                .get('http://api.alquran.cloud/v1/page/' + this.$route.params.page + '/quran-uthmani')
+                .then(response => (this.texts = response.data.data.ayahs))
+        }
 
-      };
-    },
-    methods: {
-      text: function(){
-        const axios = require('axios');
-        axios.get('http://api.alquran.cloud/v1/page/'+this.page+'/quran-unicode')
-                .then(function (response) {
-                  // handle success
-                  console.log(response.data.data.ayahs[0].text);
-                  return response.data.data.ayahs[0].text
-                })
-                .catch(function (error) {
-                  // handle error
-                  console.log(error);
-                })
-      }
     }
-  }
 </script>
